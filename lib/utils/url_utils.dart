d28f5c59bd30237d0aa48b035c13554f0624bb4f
Eduard Kutermin <82903497+HerpDerper@users.noreply.github.com>
2023-08-22 18:34:56 +0300

    ''

--- lib/utils/url_utils.dart
+++ lib/utils/url_utils.dart
@@ -1,11 +1,11 @@
 enum URL {
-  user('http://192.168.0.100:8888/user'),
-  token('http://192.168.0.100:8888/token'),
-  image('http://192.168.0.100:8888/image'),
-  friend('http://192.168.0.100:8888/friend'),
-  chat('http://192.168.0.100:8888/chat'),
-  message('http://192.168.0.100:8888/message'),
-  friendRequest('http://192.168.0.100:8888/friend_request');
+  user('http://localhost:8888/user'),
+  token('http://localhost:8888/token'),
+  image('http://localhost:8888/image'),
+  friend('http://localhost:8888/friend'),
+  chat('http://localhost:8888/chat'),
+  message('http://localhost:8888/message'),
+  friendRequest('http://localhost:8888/friend_request');
 
   final String value;
 
